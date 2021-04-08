fae0276f19ecd1099db20aa9f65b7ebb8e636b96
valentina comerci <valentinacomerci3@gmail.com>
2021-04-08 05:04:51 -0700

    Navigation part2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,21 +1,17 @@
-import React, { Component } from "react";
-import Directory from "./DirectoryComponent";
+import React, { Component } from 'react';
+import Home from './HomeComponent';
+import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
-//we deleted the CAMPSITE import cause we wont use it here anymore
-//main component is a central hub that holds all navigators like it held routes in react
 
-//funciton with components available for the stack in argument
 const DirectoryNavigator = createStackNavigator(
     {
         Directory: { screen: Directory },
         CampsiteInfo: { screen: CampsiteInfo }
     },
-    //optional second argument
-    //we are setting default options and style
-    //for more properties available read documentation
     {
         initialRouteName: 'Directory',
         defaultNavigationOptions: {
@@ -30,20 +26,42 @@ const DirectoryNavigator = createStackNavigator(
     }
 );
 
-//we'll pass what we created to this function
-//connects navigator to reactnative app
-const AppNavigator = createAppContainer(DirectoryNavigator);
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home }
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
 
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator },
+        Directory: { screen: DirectoryNavigator }
+    },
+    {
+        drawerBackgroundColor: '#CEC8FF'
+    }
+);
+
+const AppNavigator = createAppContainer(MainNavigator)
 
 class Main extends Component {
     render() {
         return (
-            <View
-                style={{
-                    flex: 1,
-                    //code to compensate for possible discrepancies btw android and ios
-                    paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
-                }}>
+            <View style={{
+                flex: 1,
+                paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
+            }}>
                 <AppNavigator />
             </View>
         );
