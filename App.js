4748fc79e78d2de2c0dbdf6ac6e78284c84c35b3
valentina comerci <valentinacomerci3@gmail.com>
2021-04-20 06:15:14 -0700

    Persist Redux Store

--- App.js
+++ App.js
@@ -3,14 +3,20 @@ import Main from "./components/MainComponent";
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
 
-const store = ConfigureStore();
+import { PersistGate } from 'redux-persist/es/integration/react';
+import Loading from './components/LoadingComponent';
+
+const { persistor, store } = ConfigureStore();
 
 export default function App() {
-  return (
-      <Provider store={store}>
-            <Main />
-      </Provider>
-  ); 
+    return (
+        <Provider store={store}>
+            <PersistGate
+                loading={<Loading />}
+                persistor={persistor}>
+                <Main />
+            </PersistGate>
+        </Provider>
+    );
 }
-
 
