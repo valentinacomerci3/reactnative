e387f50eb14637d857a7da118df33229d03b3750
valentina comerci <valentinacomerci3@gmail.com>
2021-04-20 04:15:32 -0700

    Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,15 +4,18 @@ import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
+import Favorites from './FavoritesComponent';
+import Reservation from './ReservationComponent';
+import SafeAreaView from 'react-native-safe-area-view';
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
 import { Icon } from 'react-native-elements';
-import SafeAreaView from 'react-native-safe-area-view';
 import { connect } from 'react-redux';
 import { fetchCampsites, fetchComments, fetchPromotions, fetchPartners } from '../redux/ActionCreators';
-import Reservation from './ReservationComponent';
+
+
 
 const mapDispatchToProps = {
     fetchCampsites,
@@ -20,6 +23,29 @@ const mapDispatchToProps = {
     fetchPromotions,
     fetchPartners
 };
+
+const FavoritesNavigator = createStackNavigator(
+    {
+        Favorites: { screen: Favorites }
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='heart'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
 
 const ReservationNavigator = createStackNavigator(
     {
@@ -233,7 +259,21 @@ const MainNavigator = createDrawerNavigator(
                     />
                 )
             }
-        }
+        },
+        Favorites: {
+            screen: FavoritesNavigator,
+            navigationOptions: {
+                drawerLabel: 'My Favorites',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='heart'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        }    
     },
     {
         drawerBackgroundColor: '#CEC8FF',
